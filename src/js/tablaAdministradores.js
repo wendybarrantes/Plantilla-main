e1a9325adb955d18be034a934ef4b9732ab85abd
wendybarrantes <wenbarrantes06@gmail.com>
2024-08-28 11:16:26 -0600

    tabla administradores

---
+++ src/js/tablaAdministradores.js
@@ -0,0 +1,32 @@
+import {traerDatos } from "../servicios/fetch"
+
+let tabla = document.getElementById("dataTable")
+
+
+ async function mostrarAdministradores() {
+    let administradores = await traerDatos("administradores")
+
+    administradores.forEach(administrador=>{
+        let tbody = document.createElement("tbody")
+        let tr = document.createElement("tr")
+        let tdNombre = document.createElement("td")
+        let tdApellido = document.createElement("td")
+        let tdSede = document.createElement("td")
+
+
+        tdNombre.textContent = administrador.Nombre
+        tdApellido.textContent = administrador.Apellido
+        tdSede.textContent = administrador.Sede
+        
+        tbody.appendChild(tr)
+
+        tr.appendChild(tdNombre)
+
+        tr.appendChild(tdApellido)
+
+        tr.appendChild(tdSede)
+
+        tabla.appendChild(tbody)
+    })
+ }
+ mostrarAdministradores()
